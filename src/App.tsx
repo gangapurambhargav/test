c503c59b5c82c485afd2bfa18f8805da8edb1365
bhargavgb <51057297+bhargavgb@users.noreply.github.com>
2020-12-11 02:40:53 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Codeutcutf Sandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
